eed1bdcd90c06480f60cf769a366248c828f0703
Petr Filla <petr.filla@zeus-solutions.cz>
2021-06-02 08:51:36 +0200

    #1: Init application

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloI18n/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HelloI18n from '@/components/HelloI18n.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld, HelloI18n
+  },
+  created () {
+    console.log(this.$router)
+  }
+}
+</script>
