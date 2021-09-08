377b6acb341308267c6bffa0374387080b901286
sladky <sladkyjan2@gmail.com>
2021-09-08 22:15:07 +0200

    Create dao preparation

--- src/views/dao/CreateNewDaoForm.vue
+++ src/views/dao/CreateNewDaoForm.vue
@@ -13,12 +13,56 @@
         </MDBModalHeader>
         <MDBModalBody>
             
-            <!-- Form -->
+            <!--
+
+            <MDBStepper vertical>
+                <MDBStepperStep active>
+                <MDBStepperHead icon="1">
+                    step1
+                </MDBStepperHead>
+                <MDBStepperContent>
+                    <span
+                    >Lorem ipsum dolor sit amet enim. Etiam ullamcorper.
+                    Suspendisse a pellentesque dui, non felis. Maecenas malesuada
+                    elit lectus felis, malesuada ultricies.</span
+                    >
+                </MDBStepperContent>
+                </MDBStepperStep>
+                <MDBStepperStep>
+                <MDBStepperHead icon="2">
+                    step2
+                </MDBStepperHead>
+                <MDBStepperContent>
+                    <span
+                    >Lorem ipsum dolor sit amet enim. Etiam ullamcorper.
+                    Suspendisse a pellentesque dui, non felis. Maecenas malesuada
+                    elit lectus felis, malesuada ultricies.</span
+                    >
+                </MDBStepperContent>
+                </MDBStepperStep>
+                <MDBStepperStep>
+                <MDBStepperHead icon="3">
+                    step3
+                </MDBStepperHead>
+                <MDBStepperContent>
+                    <span
+                    >Lorem ipsum dolor sit amet enim. Etiam ullamcorper.
+                    Suspendisse a pellentesque dui, non felis. Maecenas malesuada
+                    elit lectus felis, malesuada ultricies.</span
+                    >
+                </MDBStepperContent>
+                </MDBStepperStep>
+            </MDBStepper> -->
+
+            <label for="account-id-input" class="form-label">{{ t('default.account_id') }}</label>
+            <MDBInput id="account-id-input" aria-describedby="account-addon" v-model="account" :data-mdb-showcounter="true" maxlength="64" required/>     
+                
+            <!-- Form 
             <MDBRow tag="form" class="g-3 needs-validation" novalidate @submit.prevent="checkForm">
                 <MDBCol col="12">
                     <MDBInput
                         :label="t('default.dao_name')"
-                        v-model="daoName"
+                        v-model="account"
                         :invalidFeedback="t('default.dao_form_invalid_feedback')" 
                         :validFeedback="t('default.dao_form_valid_feedback')" 
                         required
@@ -71,7 +115,7 @@
                 </MDBCol>
             </MDBRow>
 
-            <!-- /Form -->
+              Form -->
 
 
             <!-- <form class="needs-validation" novalidate  @submit.prevent="checkForm"> 
@@ -145,10 +189,11 @@
     MDBModalTitle,
     MDBModalBody,
     //MDBModalFooter,
-    MDBTextarea,
+    //MDBTextarea,
     MDBInput,
-    MDBRow,
-    MDBCol,
+    //MDBRow,
+    //MDBCol,
+    //MDBStepper, MDBStepperStep, MDBStepperHead, MDBStepperContent
   } from 'mdb-vue-ui-kit';
 
 export default({
@@ -159,26 +204,114 @@ export default({
         MDBModalTitle,
         MDBModalBody,
         //MDBModalFooter,
-        MDBTextarea,
+        //MDBTextarea,
         MDBInput,
-        MDBRow,
-        MDBCol
+        //MDBRow,
+        //MDBCol,
+        //MDBStepper, MDBStepperStep, MDBStepperHead, MDBStepperContent
     },
     setup() {
         const { t } = useI18n();
-        const exampleModal = ref(false);        
-        const daoName = ref('');
-        const daoPurpose = ref('');
-        const daoCouncil = ref('');
+        const exampleModal = ref(false);
+        // basic        
+        const account = ref('');  // TODO: podilni.near, not dot
+        const name = ref('') // nazev dao 3 .. 64 TODO: unique dao name
+        const description = ref('') // textare max 3000
+        const councils = ref([]) // at least 1 root account something.near
+        
+        // tokens
+        const ftName = ref('')   // governance token 
+        const ftAmount = ref(0)
+        const ftInsiderInitDistribution = ref(0) // 0 ... ftAmount
+        const ftInsiderShare = ref(0) // 0 ... 100 all shareing 
+        const ftFundationShare = ref(0) // 0 ... 100 all shareing 
+        const ftCommunityShare = ref(0) // 0 ... 100 all shareing 
+        const ftPublicShare = ref(100) // 0 ... 100 all shareing
+        // voting 
+        const voteSpamTreshold = ref(0) // 0 .. 100 
+        const voteDurationDays = ref(0)
+        const voteDurationHours = ref(0)
+        const voteQuorum = ref(50) // 10 ... 100
+        const voteApproveTreshhold = ref(50) // 0 .. 100
+        const voteOnlyOnce = ref(true)
+        
+
+
+        const daoPurpose = ref('')
+        const daoCouncil = ref('')
         const daoTags = ref('')
-        const daoInitialTreasury = ref()
+        const daoInitialTreasury = ref(0)
 
         const checkForm = (e) => {
             e.target.classList.add('was-validated');
         };
         return{
-           t, exampleModal, daoName, daoPurpose, daoCouncil, daoTags, daoInitialTreasury, checkForm
+           t, exampleModal, account, daoPurpose, daoCouncil, daoTags, daoInitialTreasury, checkForm, name, 
+           description, ftName, ftAmount, ftInsiderInitDistribution, ftInsiderShare, ftFundationShare, ftCommunityShare,
+           ftPublicShare, voteSpamTreshold, voteDurationDays, voteDurationHours, voteQuorum, voteApproveTreshhold, voteOnlyOnce,
+           councils 
         } 
+        
     },
+    methods:{
+        createDao(){
+            const accountId = this.account // all
+            const publicKey = null
+            const info = {
+                name: this.name,
+                description: this.description,
+                ft_name: this.ftName,
+                ft_amount: this.ftAmount, // 1.. 1_000_000_000
+                tags: ['organization', 'dao']
+            }
+            // const args = Buffer.from(JSON.stringify({
+            const args = {
+                name: this.name,
+                total_supply: this.ftAmount, // TODO: check 
+                init_distribution: this.ftInsiderInitDistribution,
+                ft_metadata: {
+                    spec: "ft-1.0.0",
+                    name: this.ftName,
+                    symbol: this.account.toUpperCase(),
+                    icon: null,
+                    reference: null,
+                    reference_hash: null,
+                    decimals: 0
+                }
+            }
+            // })).toString('base64')
+            const config = {
+                insiders_share: this.ftInsiderShare,
+                fundation_share: this.ftFundationShare,
+                community_share: this.ftCommunityShare,
+                description: this.description,
+                vote_spam_treshold: this.voteSpamTreshold,
+                release_config: 'voting',
+            }
+            const vote_policy_configs = [
+                JSON.stringify({
+                    proposal_kind: '...', // TODO: PStu fill in
+                    duration: (this.voteDurationHours * 3_600) + (this.voteDurationDays * 3_600 * 24),
+                    quorum: this.voteQuorum,
+                    approve_treshhold: this.voteApproveTreshhold,
+                    vote_only_once: this.voteOnlyOnce 
+                })
+            ]
+            const founders = this.councils
+            
+            console.log(accountId, publicKey, info, args, config, vote_policy_configs, founders)
+        }
+
+    },
+
+    computed: {
+        accountId(){
+            return this.$store.getters['near/getAccountId']
+        }
+    },
+    
+    mounted(){
+        this.councils = [this.accountId]
+    }
 })
 </script>
