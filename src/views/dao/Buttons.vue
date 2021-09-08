fc61a0b1913d19258fbcbe91fb9d1519c7566e8a
Petr Filla <petr.filla@zeus-solutions.cz>
2021-09-08 09:54:46 +0200

    #3: Header changes and form for voting

--- src/views/dao/Buttons.vue
+++ src/views/dao/Buttons.vue
@@ -1,7 +1,7 @@
 <template>
-    <section class="d-flex justify-content-between align-items-center py-3">
+    <section class="row d-flex justify-content-between align-items-center py-3">
       <!-- Left -->
-      <div>
+      <div class="col-12 col-lg-9">
         <router-link :to="{ name: 'dao', query: { id: id, page: 'overview' }}" :class="[isActive('overview') ? 'bg-light' : 'text-reset']" class="btn btn-link btn-lg px-3" data-mdb-ripple-color="dark">
           {{ t('default.overview') }}
         </router-link>
@@ -24,29 +24,90 @@
       <!-- Left -->
 
       <!-- Right -->
-      <div class="d-none d-lg-block">
-        <button type="button" class="btn btn-light bg-light px-3 me-2" data-mdb-ripple-color="dark">
+      <div class="col-12 col-lg-3">
+        <!--<button type="button" class="btn btn-light bg-light px-3 me-2" data-mdb-ripple-color="dark">
           <i class="fas text-warning fa-star"></i>
-        </button>
+        </button>-->
         <!--<button type="button" class="btn btn-light bg-light px-3 me-2" data-mdb-ripple-color="dark">
           <i class="fas fa-ellipsis-h"></i>
         </button>-->
+        <MDBBtn aria-controls="modalPayout" @click="modalPayout = true" class="btn btn-primary px-3 me-2" data-mdb-ripple-color="dark">
+          <MDBIcon icon="paper-plane pe-2"/> {{ t('default.payout')}}
+        </MDBBtn>
       </div>
-      <!-- Right -->
+      <!-- /Right -->
     </section>
+
+    <!-- MODALS -->
+    <MDBModal
+      id="modalPayout"
+      tabindex="-1"
+      labelledby="modalPayoutLabel"
+      v-model="modalPayout"
+    >
+      <MDBModalHeader>
+        <MDBModalTitle id="modalPayoutLabel"> {{ t('default.payout') }} </MDBModalTitle>
+      </MDBModalHeader>
+      <MDBModalBody class="text-start">
+        <label for="account-id-input" class="form-label">{{ t('default.account_id') }}</label>
+        <MDBInput id="account-id-input" inputGroup :formOutline="false" aria-describedby="account-addon" v-model="modalPayoutAccount" :data-mdb-showcounter="true" maxlength="64" required>
+          <span class="input-group-text" id="account-addon">.near</span>
+        </MDBInput>
+        <br/>
+        <label for="amount-input" class="form-label">{{ t('default.amount') }}</label>
+        <MDBInput class="text-left" id="amount-input" min="0.00" inputGroup :formOutline="false" aria-describedby="amount-addon" type="number" v-model="modalPayoutAmount" required>
+          <span class="input-group-text" id="amount-addon">Ⓝ</span>
+        </MDBInput>
+      </MDBModalBody>
+      <MDBModalFooter>
+        <MDBBtn color="secondary" @click="modalPayoutClose()">{{ t('default.close') }}</MDBBtn>
+        <MDBBtn color="primary" @click="modalPayoutVote()">{{ t('default.vote') }}</MDBBtn>
+      </MDBModalFooter>
+    </MDBModal>
+    <!-- /MODALS -->
 </template>
 
 <script>
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
+import {
+  MDBBtn,
+  MDBInput,
+  MDBIcon,
+  MDBModal,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter
+} from "mdb-vue-ui-kit";
 
 export default {
+  components: {
+    MDBBtn, MDBIcon, MDBInput,
+    MDBModal, MDBModalHeader, MDBModalTitle, MDBModalBody, MDBModalFooter
+  },
   setup() {
     const { t } = useI18n();
-    return { t };
+    const modalPayout = ref(false)
+    const modalPayoutAccount = ref('')
+    const modalPayoutAmount = ref(0)
+
+    return {
+      t,
+      modalPayout, modalPayoutAccount, modalPayoutAmount
+    };
   },
   methods: {
     isActive(button_page) {
       return button_page === (this.$route.query.page || 'overview')
+    },
+    modalPayoutVote() {
+      this.modalPayoutAccount = null
+      this.modalPayoutAmount = null
+      this.modalPayout = false
+    },
+    modalPayoutClose() {
+      this.modalPayout = false
     }
   }
 };
