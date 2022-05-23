699efc0dbb3349592014597844f9f2dcb930351a
sladky <sladkyjan2@gmail.com>
2022-05-23 10:59:19 +0200

    change Dao wallet in title

--- src/components/dao/Title.vue
+++ src/components/dao/Title.vue
@@ -4,7 +4,37 @@
     <div class="bg-image title-background-image position-relative">
       <div class="mask">
         <div class="d-flex position-absolute top-0 end-0 me-4 mt-3">
-          <MDBBtn tag="router-link" :to="{ name: 'dao', params: {id: dao.wallet}, query: {page: 'wallet_account' }}" size="sm" rounded class="bg-gradient-110 text-white me-2" >
+
+        <MDBDropdown btnGroup v-model="dropdown1" >
+          <MDBBtn color="primary" tag="router-link" :to="{ name: 'dao', params: {id: dao.wallet}, query: {page: 'wallet_account' }}" size="sm" class="bg-gradient-110">
+            <i class="bi bi-wallet2 me-2"/>{{t('default.dao_wallet')}}
+          </MDBBtn>
+          <MDBDropdownToggle split @click="dropdown1 = !dropdown1" size="sm" class="bg-gradient-110" />
+          <MDBDropdownMenu>
+            <MDBDropdownItem tag="router-link" :to="{ name: 'dao', params: {id: dao.wallet}, query: {page: 'about' }}">
+              {{ t('default.about') }}
+            </MDBDropdownItem>
+          </MDBDropdownMenu>
+        </MDBDropdown>
+<!-- 
+          <MDBBtnGroup size="sm">
+            <MDBBtn color="primary" tag="router-link" :to="{ name: 'dao', params: {id: dao.wallet}, query: {page: 'wallet_account' }}" class="text-white" >
+              <i class="bi bi-wallet2 me-2"/>{{t('default.dao_wallet')}}
+            </MDBBtn>
+            <MDBBtnGroup size="sm" id="dropdown-target">
+              <MDBBtn color="primary" @click="dropdown1 = !dropdown1" class="dropdown-toggle text-white"/>
+              <MDBDropdown v-model="dropdown1">
+                <MDBDropdownMenu>
+                  <MDBDropdownItem tag="router-link" :to="{ name: 'dao', params: {id: dao.wallet}, query: {page: 'about' }}">
+                    {{ t('default.about') }}
+                  </MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBBtnGroup>
+          </MDBBtnGroup> -->
+
+
+          <!-- <MDBBtn tag="router-link" :to="{ name: 'dao', params: {id: dao.wallet}, query: {page: 'wallet_account' }}" size="sm" rounded class="bg-gradient-110 text-white me-2" >
             <i class="bi bi-wallet2 me-2"/>{{t('default.dao_wallet')}}
           </MDBBtn>
             <MDBDropdown v-model="dropdown1">
@@ -18,7 +48,7 @@
                   {{ t('default.dApps') }}
                 </MDBDropdownItem>
               </MDBDropdownMenu>
-            </MDBDropdown>
+            </MDBDropdown> -->
         </div>
 
         <div class="d-flex justify-content-center align-items-center h-100">
@@ -35,7 +65,7 @@
 </template>
 
 <script>
-import { MDBBtn, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdb-vue-ui-kit";
+import { MDBBtn, MDBDropdown, MDBDropdownMenu, MDBDropdownItem, MDBDropdownToggle } from "mdb-vue-ui-kit";
 import { useI18n } from 'vue-i18n';
 import { ref } from '@vue/reactivity';
 
@@ -43,9 +73,9 @@ export default {
   components: {
     MDBBtn,
     MDBDropdown, 
-    MDBDropdownToggle, 
     MDBDropdownMenu, 
-    MDBDropdownItem
+    MDBDropdownItem,
+    MDBDropdownToggle
   },
   props: {
     dao: {
