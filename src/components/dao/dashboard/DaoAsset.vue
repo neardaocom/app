32b0a5d40b5a1a4178fef709162339b6a15f537a
sladky <sladkyjan2@gmail.com>
2022-05-29 12:24:44 +0200

    About to button in title

--- src/components/dao/dashboard/DaoAsset.vue
+++ src/components/dao/dashboard/DaoAsset.vue
@@ -3,16 +3,17 @@
       <MDBCardBody>
          <div class="d-flex">
             <div class="d-flex align-items-center">
-               <Icon icon="NEAR" :size="50"/>
+               <Icon v-if="suffix==='NEAR'"  icon="NEAR" :size="50"/>
+               <Icon v-else :icon="icon" :size="50"/>
                <div class="text-start">
-                  <div class="fs-5 fw-bold me-1"> NEAR </div>
-                  <div class="text-muted small mt-n2">token</div>
+                  <div class="fs-5 fw-bold me-1"> {{accountId}} </div>
+                  <div class="text-muted small mt-n2">{{type}}</div>
                </div>
             </div>
 
             <div class="ms-auto text-end">
-               <span class="fs-5 fw-bold"><NumberFormatter :amount="1000"/></span><span class="fs-5 ps-1">NEAR</span>
-               <div class="text-end"><span class="fw-bold"><NumberFormatter :amount="1000"/></span><span class="ps-1">USD</span></div>
+               <span class="fs-5 fw-bold"><NumberFormatter :amount="1000"/></span><span class="fs-5 ps-1">{{suffix}}</span>
+               <div v-if="usd" class="text-end"><span class="fw-bold"><NumberFormatter :amount="1000"/></span><span class="ps-1">USD</span></div>
             </div>
          </div>
          
@@ -35,6 +36,33 @@ export default {
       MDBCardBody,
       Icon,
       NumberFormatter
+   },
+   props: {
+      type:{
+         type: String,
+         required: false
+      },
+      icon:{
+         type: String,
+         required: false
+      },
+      accountId:{
+         type: String,
+         required: false
+      },
+      amount:{
+         type: [Number, String],
+         required: true
+      },
+      suffix:{
+         type: String,
+         required: true
+      },
+      usd:{
+         type: Boolean,
+         required: false,
+         default: false
+      },
    },
    setup () {
       const {t} = useI18n()
--- src/pages/dao/Dashboard.vue
+++ src/pages/dao/Dashboard.vue
@@ -19,17 +19,17 @@
     </div> 
 
     <h5 class="text-start">{{t('default.dao_assets')}}</h5>
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-12 col-md-6 col-lg-5 mb-4">
-        <DaoAsset />
+        <DaoAsset  :accountId="treasuryNear.asset.symbol" :amount="availableNearAmount" :suffix="treasuryNear.asset.symbol" :icon="treasuryNear.asset.icon" type="Token"/>
       </div>
       <div class="col-12 col-md-6 col-lg-5 mb-4">
-        <DaoAsset />
+        <DaoAsset  :accountId="treasuryToken.asset.accountId" :amount="availableTokenAmount" :suffix="treasuryToken.asset.symbol" :icon="treasuryToken.asset.icon" type="Token"/>
       </div>
-      <div class="col-12 col-md-6 col-lg-5 mb-4">
-        <DaoAsset />
+      <div v-for="(ftAsset, index) in treasuryFtAssets" :key="index" class="col-12 col-md-6 col-lg-5 mb-4">
+        <DaoAsset :accountId="ftAsset.asset.accountId" :amount="ftAsset.amount - ftAsset.amountLockedInLocks" :suffix="ftAsset.asset.symbol" :icon="ftAsset.asset.icon" type="Token"/>
       </div>
-    </div>
+    </div> -->
 
     <hr/>
     <div class="row mt-2">
@@ -60,8 +60,9 @@ import _ from "lodash"
 // import loFind from "lodash/find"
 import DashboardOverview from '../../components/dao/dashboard/DashboardOverview.vue'
 import ActiveProposals from '@/components/dao/dashboard/ActiveProposals.vue'
-import DaoAsset from '@/components/dao/dashboard/DaoAsset.vue'
+// import DaoAsset from '@/components/dao/dashboard/DaoAsset.vue'
 import Incentives from '@/components/dao/dashboard/Incentives.vue';
+import { useAnalytics } from '@/hooks/treasury';
 
 
 
@@ -73,7 +74,7 @@ export default {
     DashboardOverview,
     GovernanceToken,
     ActiveProposals,
-    DaoAsset,
+    // DaoAsset,
     Incentives
   },
   props: {
@@ -93,8 +94,9 @@ export default {
   setup() {
     const { t, n } = useI18n();
     const dao = inject('dao')
+    const loader = inject('loader')
     // const { walletId, walletRights, daoRights } = toRefs(props)
-
+    const { dataLoaded, treasuryLocks, treasuryTotalAssets, treasuryNear, treasuryToken, treasuryFtAssets, availableNearAmount,  availableTokenAmount } = useAnalytics(dao, loader)
     const skywardSaleIds = ref(Auction.getSkywardSaleIds(dao.value.storage))
     // console.log(dao, walletId, walletRights, daoRights, d)
     // proposals
@@ -107,7 +109,10 @@ export default {
 
 
     return {
-      dao, t, n, proposals, skywardSaleIds
+      dao, t, n, proposals, skywardSaleIds,
+      treasuryNear, availableNearAmount, treasuryFtAssets,
+      treasuryToken, treasuryTotalAssets, treasuryLocks,
+      dataLoaded, availableTokenAmount
     };
   },
   computed: {
